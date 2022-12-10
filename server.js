76835ce0a0760c113bd0be78fab692e7e7f38484
Cgotts <cobi.gottschalk@gmail.com>
2022-12-10 13:28:16 -0800

    Added code for express data parsing

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path');
 const app = express();
 const PORT = 4000;
 
@@ -10,9 +11,34 @@ const PORT = 4000;
 
 
 
+// Api routes
+
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
 
 
 
+
+// app.get('*', (req, res) =>
+//   res.sendFile(path.join(__dirname, 'public/index.html'))
+// );
+
+
+
+// Gets the index html (Need to have * in place of /index)
+app.get('/index', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+
+// Gets the notes hmtl
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
+
+// Listens for app at port
 app.listen(PORT, () =>
-  console.log(`Example app listening at http://localhost:${PORT}`)
+  console.log(`Notetaker app listening at http://localhost:${PORT}`)
 );
